812724802c3b47ecf5775442a737f35500b5e90f
osro2011 <osro2011@edu.laroverken.se>
2022-01-17 14:22:04 +0100

    Add project files.

---
+++ Gymnasiearbete/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gymnasiearbete.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
