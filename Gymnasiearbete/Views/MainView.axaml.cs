b3619ecd75ec6151f9b1a8e6fe4244f2a0c40908
Oscar Rosenlund <oscros03@gmail.com>
2022-01-22 20:44:50 +0100

    created shape from code

--- Gymnasiearbete/Views/MainView.axaml.cs
+++ Gymnasiearbete/Views/MainView.axaml.cs
@@ -1,30 +1,41 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using System.Collections.Generic;
 
 namespace Gymnasiearbete.Views
 {
     public partial class MainView : UserControl
     {
-        public List<Shape> Shapes { get; set; }
+        public List<Shape>? Shapes { get; set; }
+        public Rectangle TestRect { get; set; }
+        public Canvas MainCanvas { get; set; }
         public MainView()
         {
-            
-
-            DrawShapes();
             InitializeComponent();
+
+            MainCanvas = this.FindControl<Canvas>("MainCanvas");
+
+            TestRect = new Rectangle
+            {
+                Fill = new SolidColorBrush
+                {
+                    Color = new Color(255, 255, 0, 0)
+                },
+                Height = 200,
+                Width = 200
+            };
+            MainCanvas.Children.Add(TestRect);
+            Canvas.SetTop(TestRect, 20);
+            Canvas.SetLeft(TestRect, 20);
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        private void DrawShapes ()
-        {
-
         }
     }
 }
