b50e66b5fc2eb018343eda7538fa365f7b84845c
Oscar Rosenlund <oscros03@gmail.com>
2022-01-23 04:40:11 +0100

    Physics rectangle added

---
+++ Gymnasiearbete/Models/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media;
+
+namespace Gymnasiearbete.Models
+{
+    public class Rectangle
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Color Color { get; set; }
+
+        public Rectangle(double x, double y, int height, int width, Color color)
+        {
+            X = x;
+            Y = y;
+            Height = height;
+            Width = width;
+            Color = color;
+        }
+    }
+}
--- Gymnasiearbete/Views/MainView.axaml.cs
+++ Gymnasiearbete/Views/MainView.axaml.cs
@@ -4,33 +4,77 @@ using Avalonia.Controls.Shapes;
 using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
 
 namespace Gymnasiearbete.Views
 {
     public partial class MainView : UserControl
-    {
-        public List<Shape>? Shapes { get; set; }
-        public Rectangle TestRect { get; set; }
+    { 
+        private Stopwatch Timer { get; set; }
+        private long CurrentElapsed { get; set; }
+        private long LastElapsed { get; set; }
+        private int DeltaTime { get; set; }
+
+        public ObservableCollection<Models.Rectangle> PhysicsShapes { get; set; }
+        public List<Shape> ControlShapes { get; set; }
         public Canvas MainCanvas { get; set; }
+
         public MainView()
         {
             InitializeComponent();
 
+            PhysicsShapes = new ObservableCollection<Models.Rectangle>();
+            ControlShapes = new List<Shape>();
+            Timer = new Stopwatch();
+            LastElapsed = 0;
+
+            PhysicsShapes.CollectionChanged += new NotifyCollectionChangedEventHandler(DrawShapes);
+
             MainCanvas = this.FindControl<Canvas>("MainCanvas");
 
-            TestRect = new Rectangle
+            PhysicsShapes.Add(new Models.Rectangle(20, 20, 20, 20, new Color(255, 255, 0, 0)));
+
+            Timer.Start(); // Move later on, should be button activated
+
+            // Needs better main loop
+            while (true)
             {
-                Fill = new SolidColorBrush
+                // Set DeltaTime and Time
+                CurrentElapsed = Timer.ElapsedMilliseconds;
+                DeltaTime = (int)CurrentElapsed - (int)LastElapsed;
+                LastElapsed = CurrentElapsed;
+
+                foreach (Models.Rectangle PhysicsShape in PhysicsShapes)
                 {
-                    Color = new Color(255, 255, 0, 0)
-                },
-                Height = 200,
-                Width = 200
-            };
-            MainCanvas.Children.Add(TestRect);
-            Canvas.SetTop(TestRect, 20);
-            Canvas.SetLeft(TestRect, 20);
+                    PhysicsShape.X = CurrentElapsed;
+                }
+            }
+        }
+
+        public void DrawShapes(object? sender, EventArgs e)
+        {
+            ControlShapes.Clear();
+            foreach (Models.Rectangle PhysicsShape in PhysicsShapes)
+            {
+                Rectangle RectangleControl = new Rectangle
+                {
+                    Fill = new SolidColorBrush
+                    {
+                        Color = PhysicsShape.Color
+                    },
+                    Height = PhysicsShape.Height,
+                    Width = PhysicsShape.Width
+                };
+                MainCanvas.Children.Add(RectangleControl);
+                Canvas.SetLeft(RectangleControl, PhysicsShape.X);
+                Canvas.SetTop(RectangleControl, PhysicsShape.Y);
+
+                ControlShapes.Add(RectangleControl);
+            }
         }
 
         private void InitializeComponent()
